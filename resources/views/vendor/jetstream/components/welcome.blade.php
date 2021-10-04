f1d8b4fadc6d9a19bc9f907a334c6c8561ba8843
marodatavision <maxrosen926@gmail.com>
2021-10-04 21:56:26 +0200

    added logos to jetstream components

--- resources/views/vendor/jetstream/components/welcome.blade.php
+++ resources/views/vendor/jetstream/components/welcome.blade.php
@@ -1,5 +1,5 @@
 <div class="p-6 sm:px-20 bg-white border-b border-gray-200">
-    <div>
+    <div class="flex justify-center">
         <x-jet-application-logo class="block h-12 w-auto" />
     </div>
 
